258779c7ad4f8aad20636f924752c7246360cfa1
Tri Nguyen <khactrist99@gmail.com>
2023-12-07 15:27:54 -0800

    implement go back button

--- app/src/main/java/com/ahmetozaydin/ecommerceapp/view/MainActivity2.kt
+++ app/src/main/java/com/ahmetozaydin/ecommerceapp/view/MainActivity2.kt
@@ -2,11 +2,21 @@ package com.ahmetozaydin.ecommerceapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.ahmetozaydin.ecommerceapp.R
 
+
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+
+        val backButton: Button = findViewById(R.id.backButton)
+
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
